bf2820e32996982b53ff00e9192461b8f0c12345
vnci12 <mohayowane@gmail.com>
2026-04-28 16:37:32 +0300

    découverte de jsx

---
+++ la-maison-jungle/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+//j'importe le composant Banner.jsx
+import Banner from './components/Banner'
+
+function App() {
+
+  return (
+
+    <>
+      <Banner />
+    </>
+  )
+}
+
+export default App
---
+++ la-maison-jungle/src/components/Banner.jsx
@@ -0,0 +1,13 @@
+/**
+ * Banner est le composant pour présenter la barre de navigation
+ * Le composant Banner retourne un message codé en HTML <h1>Bienvenue à la maison Jungle</h1>
+ */
+
+const Banner = () => {
+    return <h1>Bienvenue à la maison Jungle</h1>
+};
+
+/**
+ * J'exporte le composant Banner utilisant les mots-clés export default suivi du nom du composant
+ */
+export default Banner
